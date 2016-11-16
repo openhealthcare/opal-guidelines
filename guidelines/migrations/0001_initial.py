13280e67b3467ece72a877b25a4fe51a673d764f
fredkingham <fredkingham@gmail.com>
2016-11-16 10:56:08 +0000

    add django migrations

--- guidelines/migrations/0001_initial.py
+++ guidelines/migrations/0001_initial.py
@@ -1,56 +1,35 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
     dependencies = [
-        ('opal', '0025_merge'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('opal', '__latest__'),
     ]
 
-    def forwards(self, orm):
-        # Adding model 'Guideline'
-        db.create_table(u'guidelines_guideline', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('link', self.gf('django.db.models.fields.URLField')(max_length=200, null=True, blank=True)),
-        ))
-        db.send_create_signal(u'guidelines', ['Guideline'])
-
-        # Adding M2M table for field diagnosis on 'Guideline'
-        m2m_table_name = db.shorten_name(u'guidelines_guideline_diagnosis')
-        db.create_table(m2m_table_name, (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('guideline', models.ForeignKey(orm[u'guidelines.guideline'], null=False)),
-            ('condition', models.ForeignKey(orm[u'opal.condition'], null=False))
-        ))
-        db.create_unique(m2m_table_name, ['guideline_id', 'condition_id'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Guideline'
-        db.delete_table(u'guidelines_guideline')
-
-        # Removing M2M table for field diagnosis on 'Guideline'
-        db.delete_table(db.shorten_name(u'guidelines_guideline_diagnosis'))
-
-
-    models = {
-        u'guidelines.guideline': {
-            'Meta': {'object_name': 'Guideline'},
-            'diagnosis': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['opal.Condition']", 'symmetrical': 'False'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'link': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
-        },
-        u'opal.condition': {
-            'Meta': {'ordering': "['name']", 'object_name': 'Condition'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
-        }
-    }
-
-    complete_apps = ['guidelines']
+    operations = [
+        migrations.CreateModel(
+            name='Guideline',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('link', models.URLField(null=True, blank=True)),
+                ('source', models.CharField(max_length=200)),
+                ('diagnosis', models.ManyToManyField(help_text=b'Canonical terms only', to='opal.Condition')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='GuidelineConsultation',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('consulted', models.DateTimeField(auto_now_add=True)),
+                ('guideline', models.ForeignKey(to='guidelines.Guideline')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
