ee69740a2ad5ae66d1ba4eb640cd424a889520e4
fredkingham <fredkingham@gmail.com>
2017-07-20 15:28:20 +0100

    upgrade for python 3 and opal 0.9

--- guidelines/views.py
+++ guidelines/views.py
@@ -3,7 +3,7 @@ Views for the guidelines OPAL Plugin
 """
 from django.views.generic import TemplateView
 
-# You might find these helpful ! 
+# You might find these helpful !
 # from opal.core.views import LoginRequiredMixin, _build_json_response
 
 class SelectGuidelineModalView(TemplateView):
@@ -13,5 +13,4 @@ class SelectGuidelineModalView(TemplateView):
         context = super(SelectGuidelineModalView, self).get_context_data(**kwargs)
         from guidelines.models import Guideline
         context['guidelines'] = Guideline.objects.all()
-        print context
         return context
